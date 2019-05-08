d345488ec9495156add0153c0655638041908d62
Shahriar <me@masum.ninja>
2019-05-08 13:43:10 +0600

    PMA-1 - added cors support

---
+++ app/Http/Middleware/ModifyHeader.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ModifyHeader
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (empty($request->headers->get('Content-Type'))) {
+            $request->headers->set('Content-Type', 'application/json');
+        }
+
+        if ($request->headers->get('Accept') == '*/*') {
+            $request->headers->set('Accept', 'application/json');
+        }
+
+        $response = $next($request);
+        $response->headers->set("Access-Control-Allow-Origin", "*");
+        
+        return $response;
+    }
+}

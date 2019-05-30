5b335146c14060f2d150d5ff05ff9b39548e6d44
Ismail Hossain <ismail@reformedtech.org>
2019-05-30 10:45:54 +0600

    PMA-62 created user profile child resource

---
+++ app/Http/Requests/UserProfileChild/UpdateRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests\UserProfileChild;
+
+use App\DbModels\UserProfileChild;
+use App\Http\Requests\Request;
+
+class UpdateRequest extends Request
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'userId' => 'required|exists:users,id',
+            'gender' => 'in:'.UserProfileChild::GENDER_MALE.','.UserProfileChild::GENDER_FEMALE,
+            'name' => 'required|min:3|max:191',
+            'age' => 'integer',
+        ];
+    }
+}
---
+++ app/Http/Resources/UserProfileChildResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+class UserProfileChildResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'createdByUserId' => $this->createdByUserId,
+            'userId' => $this->userId,
+            'gender' => $this->gender,
+            'name' => $this->name,
+            'age' => $this->age,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}

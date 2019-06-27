21f6ef54e18e792878e4566761bbeabc449a5288
Ismail Hossain <ismail@reformedtech.org>
2019-06-27 14:07:53 +0600

    PMA created UserRoleFactory and modified databaserSeeder class

---
+++ database/factories/UserRoleFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(App\DbModels\UserRole::class, function (Faker $faker) {
+    return [
+        'roleId'   => App\DbModels\Role::all()->random()->id,
+        'userId'   => App\DbModels\User::all()->random()->id,
+    ];
+});

0a925c01a59e1fe6b4e3623b9ff33e235cc7943d
Shahriar Mahmood <me@masum.ninja>
2019-07-04 17:31:07 +0600

    updated base repository

--- app/Repositories/Contracts/BaseRepository.php
+++ app/Repositories/Contracts/BaseRepository.php
@@ -8,34 +8,38 @@ interface BaseRepository
      * find a resource by id
      *
      * @param mixed $id
+     * @param bool $withTrashed
      * @return \ArrayAccess|null
      */
-    public function findOne($id): ?\ArrayAccess;
+    public function findOne($id, $withTrashed = false): ?\ArrayAccess;
 
     /**
      * find a resource by criteria
      *
      * @param array $criteria
+     * @param bool $withTrashed
      * @return \ArrayAccess | null
      */
-    public function findOneBy(array $criteria): ?\ArrayAccess;
+    public function findOneBy(array $criteria, $withTrashed = false): ?\ArrayAccess;
 
     /**
      * Search All resources
      *
      * @param array $searchCriteria
+     * @param bool $withTrashed
      * @return mixed
      */
-    public function findBy(array $searchCriteria = []);
+    public function findBy(array $searchCriteria = [], $withTrashed = false);
 
     /**
      * Search All resources by any values of a key
      *
      * @param string $key
      * @param array $values
+     * @param bool $withTrashed
      * @return \IteratorAggregate | null
      */
-    public function findIn(string $key, array $values): ?\IteratorAggregate;
+    public function findIn(string $key, array $values, $withTrashed = false): ?\IteratorAggregate;
 
     /**
      * save a resource
@@ -72,4 +76,12 @@ interface BaseRepository
      */
     public function updateIn(string $key, array $values, array $data): ?\IteratorAggregate;
 
+    /**
+     * patch a resource
+     *
+     * @param array $searchCriteria
+     * @param array $data
+     * @return \ArrayAccess
+     */
+    public function patch(array $searchCriteria, array $data): \ArrayAccess;
 }

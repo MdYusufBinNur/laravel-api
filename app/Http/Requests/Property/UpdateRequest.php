be1a9b6fdd1a54cd6085320f668792ef287a5a75
Ismail Hossain <ismail@reformedtech.org>
2019-07-08 16:43:22 +0600

    PMA removed unwanted quote from Property Request file

--- app/Http/Requests/Property/UpdateRequest.php
+++ app/Http/Requests/Property/UpdateRequest.php
@@ -14,7 +14,7 @@ class UpdateRequest extends Request
     public function rules()
     {
         return $rules = [
-            'companyId'  => 'exists:companies,id’',
+            'companyId'  => 'exists:companies,id',
             'title'      => 'min:3',
             'type'       => 'max:50',
             'domain'     => '',

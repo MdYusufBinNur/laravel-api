788a35105c0a5eb79ffb37ca5a6e690d1be74664
Shahriar <me@masum.ninja>
2019-07-10 17:50:39 +0600

    minor

--- database/migrations/2018_04_22_050242_create_module_option_properties_table.php
+++ database/migrations/2018_04_22_050242_create_module_option_properties_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateModuleOptionPropertyTable extends Migration
+class CreateModuleOptionPropertiesTable extends Migration
 {
     /**
      * Run the migrations.

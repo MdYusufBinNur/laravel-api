b718727954a75be20edef6680af09154470d33e7
Shahriar <me@masum.ninja>
2019-07-17 01:06:18 +0600

    PMA-95 - added necessary role checking for residents

--- app/DbModels/Resident.php
+++ app/DbModels/Resident.php
@@ -10,6 +10,8 @@ class Resident extends Model
 
     const DEFAULT_DIAL_HOME_PHONE = 'homePhone';
     const DEFAULT_DIAL_CELL_PHONE = 'cellPhone';
+    const RESIDENT_TYPE_TENANT = 'tenant';
+    const RESIDENT_TYPE_OWNER = 'owner';
 
     /**
      * The attributes that are mass assignable.
@@ -19,4 +21,55 @@ class Resident extends Model
     protected $fillable = [
         'createdByUserId', 'propertyId', 'userId', 'unitId', 'contactEmail', 'type', 'group', 'boardMember', 'sendEmailPermission', 'displayUnit', 'displayPublicProfile', 'allowPostNote', 'allowSendMessage', 'defaultDial', 'homePhone', 'cellPhone', 'employerName', 'employerAddress', 'businessPhone', 'businessEmail', 'secondaryAddress', 'secondaryPhone', 'secondaryEmail', 'joiningDate'
     ];
+
+    /**
+     * get the user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'userId');
+    }
+
+    /**
+     * get the property related to the user's role
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\hasOne
+     */
+    public function property()
+    {
+        return $this->hasOne(Property::class, 'id', 'propertyId');
+    }
+
+    /**
+     * is a tenant type resident
+     *
+     * @return bool
+     */
+    public function isTypeTenant()
+    {
+        return $this->type === self::RESIDENT_TYPE_TENANT;
+    }
+
+    /**
+     * is a owner type resident
+     *
+     * @return bool
+     */
+    public function isTypeOwner()
+    {
+        return $this->type === self::RESIDENT_TYPE_OWNER;
+    }
+
+    /**
+     * has the resident access to the property
+     *
+     * @param int $propertyId
+     * @return bool
+     */
+    public function hasAccessToTheProperty(int $propertyId)
+    {
+        return $this->propertyId === $propertyId;
+    }
 }

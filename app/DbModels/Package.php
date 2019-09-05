8ff88d14cba38d7ec26a1d3f7d70b5fb35d3c020
Ismail Hossain <ismail@reformedtech.org>
2019-09-05 18:55:47 +0600

    added required resource return value

--- app/DbModels/Package.php
+++ app/DbModels/Package.php
@@ -27,4 +27,35 @@ class Package extends Model
         'notifiedByText' => 'boolean',
         'notifiedByVoice' => 'boolean',
     ];
+
+    /**
+     * user and enterprise user relationship
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function unit()
+    {
+        return $this->hasOne(Unit::class,'id','unitId');
+    }
+
+    /**
+     * user and enterprise user relationship
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function resident()
+    {
+        return $this->hasOne(Resident::class,'id','residentId');
+    }
+
+    /**
+     * user and enterprise user relationship
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function type()
+    {
+        return $this->hasOne(PackageType::class,'id','typeId');
+    }
+
 }

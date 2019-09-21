e9ac55ecd0a8880a614d56e84659a4a4be27fdb3
Shahriar <me@masum.ninja>
2019-09-21 23:17:37 +0600

    PMA-145 - PostComment update

--- app/Http/Requests/PostComment/UpdateRequest.php
+++ app/Http/Requests/PostComment/UpdateRequest.php
@@ -15,12 +15,8 @@ class UpdateRequest extends Request
     public function rules()
     {
         return [
-            'postId' =>  'exists:posts,id',
-            'createdUserId' =>  'exists:users,id',
-            'deletedUserId' =>  'exists:users,id',
             'status' =>  'in:'. PostComment::STATUS_POSTED. ','. PostComment::STATUS_APPROVED. ','. PostComment::STATUS_PENDING. ','. PostComment::STATUS_DENIED,
             'text' =>  'min:3|max:1024',
-            'active' =>  'boolean',
         ];
     }
 }

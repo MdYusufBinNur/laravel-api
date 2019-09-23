94cdd534eb4b594de82706d371365a5cd6f4f41e
Shahriar <me@masum.ninja>
2019-09-23 17:58:26 +0600

    PMA-144 - added group constants from messages table

--- app/DbModels/Message.php
+++ app/DbModels/Message.php
@@ -8,6 +8,16 @@ class Message extends Model
 {
     use CommonModelFeatures;
 
+    const GROUP_ENTIRE_COMMUNITY = 'entire_community';
+    const GROUP_REGISTERED_USERS = 'registered_users';
+    const GROUP_UNREGISTERED_USERS = 'registered_users';
+    const GROUP_ALL_STAFFS = 'all_staffs';
+    const GROUP_SPECIFIC_TOWER = 'specific_tower';
+    const GROUP_SPECIFIC_FLOOR = 'specific_floor';
+    const GROUP_SPECIFIC_LINE = 'specific_line';
+    const GROUP_TENANTS = 'specific_tenants';
+    const GROUP_OWNERS = 'specific_owners';
+
     /**
      * The attributes that are mass assignable.
      *

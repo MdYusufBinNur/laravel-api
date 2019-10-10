7c3bdbde99bb5e0f1c9116fee61065a3a11f1a37
Ismail Hossain <ismail@reformedtech.org>
2019-10-10 11:32:16 +0600

    removed field from request

--- app/Http/Requests/PackageType/UpdateRequest.php
+++ app/Http/Requests/PackageType/UpdateRequest.php
@@ -14,7 +14,6 @@ class UpdateRequest extends Request
     public function rules()
     {
         return [
-            'propertyId' => 'exists:properties,id',
             'title' => 'min:3|max:191',
             'icon' => 'integer',
         ];

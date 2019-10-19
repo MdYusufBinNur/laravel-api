1de27c453f1cfd22e0241e01be5518becfc393fd
Shahriar <me@masum.ninja>
2019-10-19 19:01:11 +0600

    updated database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -62,7 +62,10 @@ class DatabaseSeeder extends Seeder
         factory(App\DbModels\UserProfileLink::class, 20)->create();
         factory(App\DbModels\UserProfilePost::class, 20)->create();
 
-        factory(App\DbModels\VisitorType::class, 5)->create();
+        factory(App\DbModels\VisitorType::class)->create(['title' => 'doctor']);
+        factory(App\DbModels\VisitorType::class)->create(['title' => 'generic']);
+        factory(App\DbModels\VisitorType::class)->create(['title' => 'engineer']);
+        factory(App\DbModels\VisitorType::class)->create(['title' => 'electrician']);
         factory(App\DbModels\Visitor::class, 20)->create();
         factory(App\DbModels\VisitorArchive::class, 5)->create();
 

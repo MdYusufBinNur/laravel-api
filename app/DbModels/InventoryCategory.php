df798701f61be585f3b8267c3085731c9fcc4e22
Ismail Hossain <ismail@reformedtech.org>
2019-11-03 18:33:48 +0600

    added doc

--- app/DbModels/InventoryCategory.php
+++ app/DbModels/InventoryCategory.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class InventoryCategory extends Model
 {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'title'
     ];
--- app/DbModels/InventoryItemLog.php
+++ app/DbModels/InventoryItemLog.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class InventoryItemLog extends Model
 {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'inventoryItemId',
         'propertyId',

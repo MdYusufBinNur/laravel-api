5efa9dcbbcc92b92c2892eb6105644c058422b26
Ismail Hossain <ismail@reformedtech.org>
2019-12-01 13:43:52 +0600

    remove unnecessary function

--- app/Events/ModuleSettingProperty/ModuleSettingPropertyCreatedEvent.php
+++ app/Events/ModuleSettingProperty/ModuleSettingPropertyCreatedEvent.php
@@ -36,15 +36,4 @@ class ModuleSettingPropertyCreatedEvent
         $this->moduleSettingProperty = $moduleSettingProperty;
         $this->options = $options;
     }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
-    }
-
 }
--- app/Events/ModuleSettingProperty/ModuleSettingPropertyUpdatedEvent.php
+++ app/Events/ModuleSettingProperty/ModuleSettingPropertyUpdatedEvent.php
@@ -31,15 +31,4 @@ class ModuleSettingPropertyUpdatedEvent
         $this->moduleSettingProperty = $moduleSettingProperty;
         $this->options = $options;
     }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
-    }
-
 }

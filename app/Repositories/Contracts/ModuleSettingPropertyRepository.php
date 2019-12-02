4ab2779429e1e7727b72864087e8459cd660ddbc
Shahriar <me@masum.ninja>
2019-12-02 16:37:43 +0600

    return only active modules of a property

--- app/Repositories/Contracts/ModuleSettingPropertyRepository.php
+++ app/Repositories/Contracts/ModuleSettingPropertyRepository.php
@@ -7,12 +7,19 @@ namespace App\Repositories\Contracts;
 interface ModuleSettingPropertyRepository extends BaseRepository
 {
     /**
+     * get active modules' ids by propertyId
+     *
+     * @param int $propertyId
+     * @return array
+     */
+    public function getActiveModuleIdsByPropertyId(int $propertyId);
+
+    /**
      * set module property
      *
      * @param array $data
      * @return \ArrayAccess
      */
     public function setModuleSettingProperty(array $data): \ArrayAccess;
-
 
 }
--- app/Repositories/EloquentModuleRepository.php
+++ app/Repositories/EloquentModuleRepository.php
@@ -5,8 +5,23 @@ namespace App\Repositories;
 
 
 use App\Repositories\Contracts\ModuleRepository;
+use App\Repositories\Contracts\ModuleSettingPropertyRepository;
 
 class EloquentModuleRepository extends EloquentBaseRepository implements ModuleRepository
 {
+    /**
+     * @inheritDoc
+     */
+    public function findBy(array $searchCriteria = [], $withTrashed = false)
+    {
+        if (isset($searchCriteria['propertyId'])) {
+            $moduleSettingPropertyRepository = app(ModuleSettingPropertyRepository::class);
+            $moduleIds = $moduleSettingPropertyRepository->getActiveModuleIdsByPropertyId($searchCriteria['propertyId']);
+            $searchCriteria['id'] = implode(",", $moduleIds);
+            unset($searchCriteria['propertyId']);
+        }
+
+        return parent::findBy($searchCriteria, $withTrashed);
+    }
 
 }

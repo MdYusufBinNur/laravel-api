0107e4f2c971fd5913b9bb6ccc9cf3b00feec95e
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2019-12-28 12:59:26 +0600

    modified euipment related email template

--- app/Mail/Equipment/SendMaintenanceNotification.php
+++ app/Mail/Equipment/SendMaintenanceNotification.php
@@ -34,7 +34,7 @@ class SendMaintenanceNotification extends Mailable
      */
     public function build()
     {
-        return $this->subject("Maintenance Reminder Notification!")->view('equipment.maintenance')
+        return $this->subject("Maintenance Reminder Notification!")->view('equipment.maintenance.created')
             ->with([
                 'equipment' => $this->equipment,
                 'property' => $this->equipment->property

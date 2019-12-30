4bdab18e8dd1a2c53f8c57e112771d02c56f130e
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2019-12-30 18:04:04 +0600

    registration request template modified

--- resources/views/resident/access-request/index.blade.php
+++ resources/views/resident/access-request/index.blade.php
@@ -2,11 +2,6 @@
 @section('logo')
     @include('layouts.logo')
 @endsection
-
-@section('title')
-    @include('layouts.title', ['residentAccessRequest' => $residentAccessRequest, 'property' => $property, 'unit' => $unit])
-@endsection
-
 @section('content')
-    @include('resident.access-request.body', ['data' => 'Welcome to '.$property->title])
+    @include('resident.access-request.body', ['residentAccessRequest' => $residentAccessRequest, 'property' => $property, 'unit' => $unit])
 @endsection

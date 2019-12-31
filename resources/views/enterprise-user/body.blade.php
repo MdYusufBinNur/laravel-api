08ffc37f146f5117878e2f25963bb83aad8c7166
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2019-12-31 18:03:30 +0600

    typo

--- resources/views/enterprise-user/body.blade.php
+++ resources/views/enterprise-user/body.blade.php
@@ -1,68 +1,44 @@
 <table bgcolor="#4DBFBF" cellpadding="0" cellspacing="0" class="force-full-width" style="margin: 0 auto;">
     <tbody>
     <tr>
-        <td><br>
-            <img alt="robot picture" height="240"
-                 src="https://www.filepicker.io/api/file/carctJpuT0exMaN8WUYQ"
-                 width="224">
-        </td>
-    </tr>
-
-    <tr>
-        <td class="headline"><br>
-            <strong>Greetings
-                <span id="first_name">
-                        Zakaria!!!
-                    </span>!
-            </strong>
-        </td>
-    </tr>
-
-    <tr>
         <td>
-            <center>
-                <table cellpadding="0" cellspacing="0"
-                       style="margin: 0 auto;" width="60%">
-                    <tbody>
-                    <tr>
-                        <td style="font-size:18px;color:#ffffff;">
+            <table cellpadding="0" cellspacing="0"
+                   style="margin: 0 auto;" width="80%">
+                <tbody>
+                <tr>
+                    <td height="50">
 
-                            <br>
-                            {{--todo--}}
-
-                            {{$enterpriseUser->contactEmail}}
-                            {{$user->name}}
-                            {{$company->title}}
-                            <hr>
-
-                            <hr>
-
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </center>
-        </td>
-    </tr>
-
-    <tr>
-        <td>
-            <div>
-                <!--[if mso]> <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="https://www.sendwithus.com" style="height:50px;v-text-anchor:middle;width:200px;" arcsize="8%" stroke="f" fillcolor="#178f8f"> <w:anchorlock></w:anchorlock> <center> <![endif]-->
-                <a href="https://reformedtech.org/"
-                   style="background-color:#178f8f;border-radius:4px;color:#ffffff;display:inline-block;font-family:Helvetica, Arial, sans-serif;font-size:16px;font-weight:bold;line-height:50px;text-align:center;text-decoration:none;width:250px;-webkit-text-size-adjust:none;">
-                    <span id="button_text">Button</span>
-                </a>
-                <!--[if mso]> </center> </v:roundrect> <![endif]-->
-            </div>
-            <br>
-            <br>
-            <br>
-            <br>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="left" style="font-size:18px;color:#ffffff; text-align: left;">
+                        <br>
+                        {{--todo--}}
+                        Hello {{$company->title}},
+                        <br>
+                        <br>
+                        <br>
+                        An Enterprise User has been created for your company.
+                        <br>
+                        <br>
+                        <br>
+                        User Details:
+                        <br>
+                        <br>
+                        Email: {{$enterpriseUser->contactEmail}}
+                        <br>
+                        Name: {{$user->name}}
+                        <br>
+                        <br>
+                        <br>
+                        Thank You<br>
+                        Team Smarty Property
+                        <br>
+                        <br>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
         </td>
     </tr>
     </tbody>
@@ -78,3 +54,4 @@
     </tr>
     </tbody>
 </table>
+

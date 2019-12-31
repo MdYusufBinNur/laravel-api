897c7c3e58cd5a795c5897ab608666b36d14604f
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2019-12-31 17:30:52 +0600

    modified inventory decreasing mail template

--- resources/views/inventory/notify-decreased/index.blade.php
+++ resources/views/inventory/notify-decreased/index.blade.php
@@ -2,11 +2,6 @@
 @section('logo')
     @include('layouts.logo')
 @endsection
-
-@section('title')
-    @include('layouts.title', [ 'data' => 'Inventory Item Decreased'])
-@endsection
-
 @section('content')
     @include('inventory.notify-decreased.body', ['inventory' => $inventory,'property' => $property])
 @endsection

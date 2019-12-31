e1d7882994b246ac247029ef234a9adb4432db22
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2019-12-31 17:22:57 +0600

    modified manager created mail template

--- resources/views/manager/index.blade.php
+++ resources/views/manager/index.blade.php
@@ -2,11 +2,6 @@
 @section('logo')
     @include('layouts.logo')
 @endsection
-
-@section('title')
-    @include('layouts.title', [ 'data' => 'Manager Created'])
-@endsection
-
 @section('content')
-    @include('manager.body', ['staff' => $staff,'user' => $user])
+    @include('manager.body', ['staff' => $staff,'user' => $user, 'property' => $property])
 @endsection

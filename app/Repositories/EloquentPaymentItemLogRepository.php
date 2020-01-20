a13db691c449f95686cf5aa96326a7af0525c5fa
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2020-01-20 15:03:24 +0600

    revert changes

--- app/Repositories/EloquentPaymentItemLogRepository.php
+++ app/Repositories/EloquentPaymentItemLogRepository.php
@@ -13,20 +13,9 @@ class EloquentPaymentItemLogRepository extends EloquentBaseRepository implements
      */
     public function findBy(array $searchCriteria = [], $withTrashed = false)
     {
-        $queryBuilder = $this->model;
-
         $searchCriteria['eagerLoad'] = ['pil.createdByUser' => 'createdByUser', 'pil.property' => 'property',  'pil.payment' => 'payment', 'pil.user' => 'user'];
 
-        $limit = !empty($searchCriteria['per_page']) ? (int)$searchCriteria['per_page'] : 15;
-        $orderBy = !empty($searchCriteria['order_by']) ? $searchCriteria['order_by'] : 'id';
-        $orderDirection = !empty($searchCriteria['order_direction']) ? $searchCriteria['order_direction'] : 'desc';
-        $queryBuilder->orderBy($orderBy, $orderDirection);
-
-        if (empty($searchCriteria['withOutPagination'])) {
-            return $queryBuilder->paginate($limit);
-        } else {
-            return $queryBuilder->get();
-        }
+        return parent::findBy($searchCriteria, $withTrashed);
     }
 
 }

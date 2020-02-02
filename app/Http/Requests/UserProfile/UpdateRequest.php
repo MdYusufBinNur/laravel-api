f059b77ac6d40ada584c50a46053ce5e40885f8e
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2020-02-02 16:59:50 +0600

    modified user profile

--- app/Http/Requests/UserProfile/UpdateRequest.php
+++ app/Http/Requests/UserProfile/UpdateRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests\UserProfile;
 use App\DbModels\UserProfile;
 use App\Http\Requests\Request;
 use App\Rules\CSVString;
+use Illuminate\Validation\Rule;
 
 class UpdateRequest extends Request
 {
@@ -15,6 +16,7 @@ class UpdateRequest extends Request
      */
     public function rules()
     {
+        $userId = $this->segment(4);
         return [
             'userId' => 'exists:users,id',
             'gender' => 'in:'.UserProfile::GENDER_FEMALE.','.UserProfile::GENDER_MALE,
@@ -27,6 +29,10 @@ class UpdateRequest extends Request
             'twitterUsername' => 'min:3|max:100',
             'aboutMe' => 'min:3|max:16777215',
             'interests' => [new CSVString()],
+
+            'user'                 => '',
+            'user.name'            => 'min:3|max:255',
+            'user.phone' => Rule::unique('users')->ignore($userId, 'id'),
         ];
     }
 }

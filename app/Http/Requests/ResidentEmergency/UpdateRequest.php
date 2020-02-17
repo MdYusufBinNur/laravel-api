e1a9e1096de1993ac3737b3ad456b2ea6a863281
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2020-02-17 19:55:24 +0600

    remove emil unique validation from resident emergencies

--- app/Http/Requests/ResidentEmergency/UpdateRequest.php
+++ app/Http/Requests/ResidentEmergency/UpdateRequest.php
@@ -20,7 +20,7 @@ class UpdateRequest extends Request
             'address' => 'min:3|max:255',
             'homePhone' => 'min:3|max:20',
             'cellPhone' => 'min:3|max:20',
-            'email' => 'unique:resident_emergencies,email|email|max:255',
+            'email' => 'email|max:255',
         ];
     }
 }

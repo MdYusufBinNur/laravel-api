0621189b061f8d5c0f36d44442ca1ecd36408b61
Shahriar <me@masum.ninja>
2020-02-20 18:55:34 +0600

    PMA-220 - fixed announcement search resource

--- app/Repositories/EloquentAnnouncementRepository.php
+++ app/Repositories/EloquentAnnouncementRepository.php
@@ -14,12 +14,40 @@ class EloquentAnnouncementRepository extends EloquentBaseRepository implements A
      */
     public function findBy(array $searchCriteria = [], $withTrashed = false)
     {
+        $queryBuilder = $this->model;
+        if (isset($searchCriteria['query'])) {
+            $queryBuilder = $queryBuilder->where('title', 'like', '%'.$searchCriteria['query'].'%');
+            unset($searchCriteria['query']);
+        }
 
-        $searchCriteria = $this->applyFilterInUserSearch($searchCriteria);
+        if (isset($searchCriteria['expireAt'])) {
+            $queryBuilder = $queryBuilder->whereDate('expireAt', '=', $searchCriteria['expireAt']);
+            unset($searchCriteria['expireAt']);
+        }
 
-        $announcements =  parent::findBy($searchCriteria, $withTrashed);
+        if(isset($searchCriteria['isExpired'])){
+            if ($searchCriteria['isExpired']) {
+                $queryBuilder = $queryBuilder->whereDate('expireAt', '<=', Carbon::now());
+            } else{
+                $queryBuilder = $queryBuilder->whereDate('expireAt', '>=', Carbon::now());
+            }
+            unset($searchCriteria['isExpired']);
+        }
 
-        return $announcements;
+        $queryBuilder = $queryBuilder->where(function ($query) use ($searchCriteria) {
+            $this->applySearchCriteriaInQueryBuilder($query, $searchCriteria);
+        });
+
+        $limit = !empty($searchCriteria['per_page']) ? (int)$searchCriteria['per_page'] : 15;
+        $orderBy = !empty($searchCriteria['order_by']) ? $searchCriteria['order_by'] : 'id';
+        $orderDirection = !empty($searchCriteria['order_direction']) ? $searchCriteria['order_direction'] : 'desc';
+        $queryBuilder->orderBy($orderBy, $orderDirection);
+
+        if (empty($searchCriteria['withOutPagination'])) {
+            return $queryBuilder->paginate($limit);
+        } else {
+            return $queryBuilder->get();
+        }
     }
 
     /**
@@ -33,39 +61,6 @@ class EloquentAnnouncementRepository extends EloquentBaseRepository implements A
             ->where('propertyId', $searchCriteria['propertyId']);
 
         return $queryBuilder->get();
-    }
-
-    /**
-     * shorten the search based on search criteria
-     *
-     * @param $searchCriteria
-     * @return mixed
-     */
-    private function applyFilterInUserSearch($searchCriteria)
-    {
-        if (isset($searchCriteria['query'])) {
-            $searchCriteria['id'] = $this->model->where('title', 'like', '%'.$searchCriteria['query'].'%')
-                ->pluck('id')->toArray();
-            unset($searchCriteria['query']);
-        }
-
-        if(isset($searchCriteria['isExpired'])){
-            if ($searchCriteria['isExpired']) {
-                $searchCriteria['id'] = $this->model->whereDate('expireAt', '<=', $searchCriteria['expireAt'])
-                    ->pluck('id')->toArray();
-            } else{
-                $searchCriteria['id'] = $this->model->whereDate('expireAt', '>=', $searchCriteria['expireAt'])
-                    ->pluck('id')->toArray();
-            }
-            unset($searchCriteria['isExpired']);
-            unset($searchCriteria['expireAt']);
-        }
-
-        if (isset($searchCriteria['id']) && is_array($searchCriteria['id'])) {
-            $searchCriteria['id'] = implode(",", array_unique($searchCriteria['id']));
-        }
-
-        return $searchCriteria;
     }
 
 }

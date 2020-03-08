fd7ebad0b03225e923ee50e69beb27c38bcf6dfa
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2020-03-08 11:44:37 +0600

    added  fillable field in model class

--- app/DbModels/CommitteeSession.php
+++ app/DbModels/CommitteeSession.php
@@ -2,9 +2,29 @@
 
 namespace App\DbModels;
 
+use App\DbModels\Traits\CommonModelFeatures;
 use Illuminate\Database\Eloquent\Model;
 
 class CommitteeSession extends Model
 {
-    //
+    use CommonModelFeatures;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'propertyId', 'committeeTypeId', 'sessionName', 'startedDate', 'endedDate'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'startedDate' => 'date_format:Y-m-d',
+        'endedDate' => 'date_format:Y-m-d',
+    ];
 }
--- app/DbModels/PropertyCommittee.php
+++ app/DbModels/PropertyCommittee.php
@@ -2,9 +2,19 @@
 
 namespace App\DbModels;
 
+use App\DbModels\Traits\CommonModelFeatures;
 use Illuminate\Database\Eloquent\Model;
 
 class PropertyCommittee extends Model
 {
-    //
+    use CommonModelFeatures;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'propertyId', 'committeeTypeId', 'committeeSessionId', 'userId', 'name', 'committeeRankId'
+    ];
 }

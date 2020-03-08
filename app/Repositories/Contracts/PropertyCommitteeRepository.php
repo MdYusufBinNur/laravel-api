f51ed32a0354eebc0ad9cb7c4d605e71c534a805
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2020-03-08 11:22:58 +0600

    created repository files

---
+++ app/Repositories/Contracts/PropertyCommitteeRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Repositories\Contracts;
+
+
+interface PropertyCommitteeRepository extends BaseRepository
+{
+
+}
---
+++ app/Repositories/EloquentCommitteeTypeRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Repositories;
+
+
+use App\Repositories\Contracts\CommitteeTypeRepository;
+
+class EloquentCommitteeTypeRepository extends EloquentBaseRepository implements CommitteeTypeRepository
+{
+
+}

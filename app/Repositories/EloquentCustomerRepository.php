4960a6951923b7c0e1b2af6ccafa1ed24b273c92
Shahriar <me@masum.ninja>
2020-03-12 13:10:38 +0600

    added query filter in customer and vendor

--- app/Repositories/EloquentCustomerRepository.php
+++ app/Repositories/EloquentCustomerRepository.php
@@ -8,5 +8,37 @@ use App\Repositories\Contracts\CustomerRepository;
 
 class EloquentCustomerRepository extends EloquentBaseRepository implements CustomerRepository
 {
+    /**
+     * @inheritdoc
+     */
+    public function findBy(array $searchCriteria = [], $withTrashed = false)
+    {
+        $queryBuilder = $this->model;
+        if (isset($searchCriteria['query'])) {
+            $queryBuilder = $queryBuilder
+                ->where('name', 'like', '%'.$searchCriteria['query'].'%')
+                ->orwhere('email', 'like', '%'.$searchCriteria['query'].'%')
+                ->orWhere('phone', 'like', '%'.$searchCriteria['query'].'%')
+                ->orWhere('address', 'like', '%'.$searchCriteria['query'].'%');
+            unset($searchCriteria['query']);
+        }
+
+
+        $queryBuilder = $queryBuilder->where(function ($query) use ($searchCriteria) {
+            $this->applySearchCriteriaInQueryBuilder($query, $searchCriteria);
+        });
+
+        $limit = !empty($searchCriteria['per_page']) ? (int)$searchCriteria['per_page'] : 15;
+        $orderBy = !empty($searchCriteria['order_by']) ? $searchCriteria['order_by'] : 'id';
+        $orderDirection = !empty($searchCriteria['order_direction']) ? $searchCriteria['order_direction'] : 'desc';
+        $queryBuilder->orderBy($orderBy, $orderDirection);
+
+        if (empty($searchCriteria['withOutPagination'])) {
+            return $queryBuilder->paginate($limit);
+        } else {
+            return $queryBuilder->get();
+        }
+    }
+
 
 }

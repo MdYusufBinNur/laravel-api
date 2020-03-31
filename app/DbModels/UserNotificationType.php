0ef611661c24f4c5562b0824ca93d60f8d41feb6
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2020-03-31 15:16:11 +0600

    typo

--- app/DbModels/UserNotificationType.php
+++ app/DbModels/UserNotificationType.php
@@ -16,6 +16,7 @@ class UserNotificationType extends Model
     const KEY_LOG = ['id' => 3, 'type' => 'key_log'];
     const LEFT_NOTES = ['id' => 4, 'type' => 'left_notes'];
     const PACKAGE = ['id' => 5, 'type' => 'package'];
+    const SERVICE_REQUEST = ['id' => 6, 'type' => 'service_request'];
     const FDI = ['id' => 7, 'type' => 'fdi'];
     const AMENITY = ['id' => 8, 'type' => 'amenity'];
     const RESIDENT_ALLOWED_TO_CONTACT = ['id' => 9, 'type' => 'residentAllowedToContact'];

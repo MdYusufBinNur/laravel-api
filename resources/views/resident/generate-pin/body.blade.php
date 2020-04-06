d8e94cdda9a5636c55559d55db060a24d31ec1f1
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2020-04-06 15:37:52 +0600

    registration pin email template

--- resources/views/resident/generate-pin/body.blade.php
+++ resources/views/resident/generate-pin/body.blade.php
@@ -1,42 +1,28 @@
 <tr>
-    <td class="body" width="100%" cellpadding="0" cellspacing="0"
-        style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #ffffff; border-bottom: 1px solid #edeff2; border-top: 1px solid #edeff2; margin: 0; padding: 0; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
-        <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0"
-               style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #ffffff; margin: 0 auto; padding: 0; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
+    <td class="email-body" width="570" cellpadding="0" cellspacing="0">
+        <table class="email-body_inner" style=" border: 1px solid #e1e1e1;" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
             <!-- Body content -->
+            <tbody>
             <tr>
-                <td class="content-cell"
-                    style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
-                    <table class="promotion" align="center" width="100%" cellpadding="0"
-                           cellspacing="0"
-                           style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #FFFFFF; border: 2px dashed #9BA2AB; margin: 0; margin-bottom: 25px; margin-top: 25px; padding: 24px; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
-                        <tr>
-                            <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                                <span
-                                    style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787e;  margin-top: 0; text-align: left; font-size: 15px; font-weight: bold;">
-                                    Login for:  {{$residentAccessRequest->name}}
-                                </span>
-                                <br>
-                                <span
-                                    style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #74787e;  margin-top: 0; text-align: left; font-size: 15px; font-weight: bold;">
-                                    Unit:  {{$unit->title}}
-                                </span >
-                                <table class="subcopy" width="100%" cellpadding="0" cellspacing="0"
-                                       style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; border-top: 1px solid #edeff2; margin-top: 25px; padding-top: 25px;">
-                                    <tr>
-                                        <td style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
-                                            <span
-                                                style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: blue;  margin-top: 0; text-align: left; font-size: 15px; font-weight: bold;">
-                                                Pin: {{$residentAccessRequest->pin}}
-                                            </span>
-                                        </td>
-                                    </tr>
-                                </table>
-                            </td>
-                        </tr>
-                    </table>
+                <td class="content-cell">
+                    <div class="f-fallback">
+                        <h1>Hey {{ $residentAccessRequest->name }}</h1>
+                        <p>
+                           Your Pin is: {{$residentAccessRequest->pin}}
+                        </p>
+                        <br>
+                        <p>
+                            Thank You
+                            <br>
+                            {{ $property->title }} Support Team
+                            <br>
+                            {{ $property->address }}
+                        </p>
+                    </div>
                 </td>
             </tr>
+            </tbody>
         </table>
     </td>
 </tr>
+

2358b1511de8a695c9dc7d657787ab5c07354866
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2020-04-06 10:55:36 +0600

    update mail main layout

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,16 +2,16 @@
     <td>
         <table class="email-footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
             <tbody>
-            <tr>
-                <td class="content-cell" align="center">
-                    <p class="f-fallback sub align-center">© 2020 ReformedTech, All rights reserved.</p>
-                    <p class="f-fallback sub align-center">
-                        SmartProperty.xyz
-                        <br>32/1, Road: 3, Shyamoli,
-                        <br>Dhaka 1207,Bangladesh
-                    </p>
-                </td>
-            </tr>
+                <tr>
+                    <td class="content-cell" align="center">
+                        <p class="f-fallback sub align-center">© 2020 ReformedTech, All rights reserved.</p>
+                        <p class="f-fallback sub align-center">
+                            SmartProperty.xyz
+                            <br>32/1, Road: 3, Shyamoli,
+                            <br>Dhaka 1207,Bangladesh
+                        </p>
+                    </td>
+                </tr>
             </tbody>
         </table>
     </td>
--- resources/views/layouts/logo.blade.php
+++ resources/views/layouts/logo.blade.php
@@ -1,9 +1,7 @@
 <tr>
-    <td height="20">
-    </td>
-</tr>
-<tr>
-    <td style="font-size: 30px; text-align:center;">
-        <img alt="company logo" class="500" height="70" src="https://reformed-stage.s3.ap-southeast-1.amazonaws.com/property-logos/tN2OrEvflJ3ACKsdxMOU_1_Logo%20Reformed%20Tech.png">
+    <td class="email-masthead">
+        <a href="https://example.com" class="f-fallback email-masthead_name">
+            <img alt="Verve Wine" src="images/logo.png" style="border-width: 0px; width: 160px;" width="160">
+        </a>
     </td>
 </tr>

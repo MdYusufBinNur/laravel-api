8f0f934592ed1c3b4983f243b9e6c0d724d63329
Shahriar <me@masum.ninja>
2020-04-07 01:17:13 +0600

    PMW-443 - logo issue

--- resources/views/layouts/logo.blade.php
+++ resources/views/layouts/logo.blade.php
@@ -11,7 +11,7 @@
     <tr>
         <td class="email-masthead">
             <a href="https://smartproperty.xyz" class="f-fallback email-masthead_name">
-                <img alt="SmartProperty Forgot Password" src="{{ asset('logo/smartproperty.png')}}" style="border-width: 0px; width: 160px;" width="160">
+                <img alt="Forgot Password" src="{{ env('BRAND_LOGO_URL')}}" style="border-width: 0px; width: 160px;" width="160">
             </a>
         </td>
     </tr>

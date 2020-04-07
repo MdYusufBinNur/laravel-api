c5d6555e863a935f8163a807a32ac2a45c084efb
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2020-04-07 16:26:07 +0600

    updated message email body template

--- resources/views/message/notification/body.blade.php
+++ resources/views/message/notification/body.blade.php
@@ -1,48 +1,18 @@
-<table bgcolor="#4DBFBF" cellpadding="0" cellspacing="0" class="force-full-width" style="margin: 0 auto;">
-    <tbody>
-    <tr>
-        <td>
-            <table cellpadding="0" cellspacing="0"
-                   style="margin: 0 auto;" width="80%">
-                <tbody>
-                <tr>
-                    <td height="50">
-
-                    </td>
-                </tr>
-                <tr>
-                    <td align="left" style="font-size:18px;color:#ffffff; text-align: left;">
-                        Dear {{ $toUser->name }}, <br> <br>
-
-                        You Have a new message from {{ $fromUser->name }}.<br>
-                        <br>
-                        <br>
-                       {{ $messageText }}
-                        <br>
-                        <br>
-                        <br>
-                        <br>
-
-                        Sincerely, <br>
-                        {{ $fromUser->name }}
-                        <br>
-                        <br>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </td>
-    </tr>
-    </tbody>
-</table>
-<table bgcolor="#F5774E" cellpadding="0" cellspacing="0" class="force-full-width" style="margin: 0 auto;">
-    <tbody>
-    <tr>
-        <td style="background-color:#f5774e;">
-            <center>
-
-            </center>
-        </td>
-    </tr>
-    </tbody>
-</table>
+<tr>
+    <td class="email-body" width="570" cellpadding="0" cellspacing="0">
+        <table class="email-body_inner" style=" border: 1px solid #e1e1e1;" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+            <!-- Body content -->
+            <tbody><tr>
+                <td class="content-cell">
+                    <div class="f-fallback">
+                        <h1>Hi {{ $toUser->name }}, </h1>
+                        <p>You have a new message</p>
+                        <p>By {{ $fromUser->name }} at {{$messageModel->created_at->toDayDateTimeString()}}</p>
+                        <p> {{ $messageText }}</p>
+{{--                        <p class="sub"><a href="{{action_url}}">View the Full Massage</a></p>--}}
+                    </div>
+                </td>
+            </tr>
+            </tbody></table>
+    </td>
+</tr>

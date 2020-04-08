3892755e8b4ef71baf30d459e15c640d39b6df75
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2020-04-08 10:52:26 +0600

    updated new manager created email body

--- resources/views/manager/body.blade.php
+++ resources/views/manager/body.blade.php
@@ -1,58 +1,69 @@
-<table bgcolor="#4DBFBF" cellpadding="0" cellspacing="0" class="force-full-width" style="margin: 0 auto;">
-    <tbody>
-    <tr>
-        <td>
-            <table cellpadding="0" cellspacing="0"
-                   style="margin: 0 auto;" width="80%">
-                <tbody>
-                <tr>
-                    <td height="50">
+<tr>
+    <td class="email-body" width="570" cellpadding="0" cellspacing="0">
+        <table class="email-body_inner" style=" border: 1px solid #e1e1e1;" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+            <!-- Body content -->
+            <tbody>
+            <tr>
+                <td class="content-cell">
+                    <div class="f-fallback">
+                        <h1> Hello {{ $property->title }} Team,</h1>
+                        <p> A staff has been added for your property</p>
+                    </div>
+                </td>
+            </tr>
+            <tr style="color:#4E5C6E; font-size:14px; line-height:20px;">
+                <td class="content" colspan="2" valign="top" align="center" style="padding-left:50px; padding-right:50px;">
+                    <table width="100%" cellpadding="0" cellspacing="0" border="0" bgcolor="#ffffff">
+                        <tbody>
+                        <tr>
+                            <td align="center">
+                                <div style="font-size: 22px; line-height: 20x; font-weight: 500; margin-left: 20px; margin-right: 20px; margin-bottom: 25px;">  Staff Details</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td height="1" bgcolor="#DAE1E9"></td>
+                        </tr>
+                        <tr>
+                            <td height="24" &nbsp;=""></td>
+                        </tr>
+                        <tr>
+                            <td align="center">
+                                <table style="width: 100%; border-collapse:collapse;">
+                                    <tbody style="border: 0; padding: 0; margin-top:20px;">
+                                    <tr>
+                                        <td style="padding-bottom: 10px; padding-top: 10px;">Name</td>
+                                        <td style="padding-bottom: 10px; padding-top: 10px;">
+                                            <h2> {{$user->name}}</h2>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding-bottom: 10px; padding-top: 10px;">Designation</td>
+                                        <td style="padding-bottom: 10px; padding-top: 10px;">  {{$user->title}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding-bottom: 10px; padding-top: 10px;">email</td>
+                                        <td style="padding-bottom: 10px; padding-top: 10px;"> {{$user->email}}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td class="content-cell">
+                    <div class="f-fallback">
+                        <p>
+                            Regards, <br>
+                            Smart Property Team
+                        </p>
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </td>
+</tr>
 
-                    </td>
-                </tr>
-                <tr>
-                    <td align="left" style="font-size:18px;color:#ffffff; text-align: left;">
-                        Hello {{ $property->title }} Team, <br> <br>
-
-                        A staff has been added for your property.<br>
-                        <br>
-                        <br>
-
-                        Staff Details:
-                        <br>
-                        <br>
-                        Name: {{$user->name}}
-                        <br>
-                        Designation: {{$user->title}}
-                        <br>
-                        email: {{$user->email}}
-                        <br>
-                        <br>
-                        If You Have any issue concerning this please contact with out support team. <br>
-
-                        <br>
-                        <br>
-
-                        Thank You<br>
-                        Team Smarty Property
-                        <br>
-                        <br>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </td>
-    </tr>
-    </tbody>
-</table>
-<table bgcolor="#F5774E" cellpadding="0" cellspacing="0" class="force-full-width" style="margin: 0 auto;">
-    <tbody>
-    <tr>
-        <td style="background-color:#f5774e;">
-            <center>
-
-            </center>
-        </td>
-    </tr>
-    </tbody>
-</table>

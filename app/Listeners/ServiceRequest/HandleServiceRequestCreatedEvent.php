16611fc3a83b759a12d9772687a152a60e06bcc7
ismailreformed <49227543+ismailreformed@users.noreply.github.com>
2020-04-15 12:26:27 +0600

    created service request created related mail class and email template

--- app/Listeners/ServiceRequest/HandleServiceRequestCreatedEvent.php
+++ app/Listeners/ServiceRequest/HandleServiceRequestCreatedEvent.php
@@ -5,9 +5,11 @@ namespace App\Listeners\ServiceRequest;
 use App\DbModels\ServiceRequestLog;
 use App\Events\ServiceRequest\ServiceRequestCreatedEvent;
 use App\Listeners\CommonListenerFeatures;
+use App\Mail\ServiceRequest\ServiceRequestCreated;
 use App\Repositories\Contracts\ServiceRequestLogRepository;
 use App\Services\Helpers\SmsHelper;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
 
 class HandleServiceRequestCreatedEvent implements ShouldQueue
 {
@@ -23,6 +25,13 @@ class HandleServiceRequestCreatedEvent implements ShouldQueue
     {
         $serviceRequest = $event->serviceRequest;
         $eventOptions = $event->options;
+
+        $unit = $serviceRequest->unit;
+        $residents = $unit->residents;
+
+        foreach ($residents as $resident) {
+            Mail::to($resident->user->email)->send(new ServiceRequestCreated($serviceRequest, $unit));
+        }
 
         $serviceRequestLogRepository = app(ServiceRequestLogRepository::class);
 
---
+++ app/Mail/ServiceRequest/ServiceRequestCreated.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Mail\ServiceRequest;
+
+use App\DbModels\ServiceRequest;
+use App\DbModels\Unit;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ServiceRequestCreated extends Mailable
+{
+    use SerializesModels;
+
+    /**
+     * @var ServiceRequest
+     */
+    public $serviceRequest;
+    /**
+     * @var Unit
+     */
+    public $unit;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param ServiceRequest $serviceRequest
+     */
+    public function __construct(ServiceRequest $serviceRequest, Unit $unit)
+    {
+        $this->serviceRequest = $serviceRequest;
+        $this->unit = $unit;
+    }
+
+    /**
+     * Send Announcement created mail to property user
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $user = $this->serviceRequest->user;
+        $category = $this->serviceRequest->category;
+        $property = $this->serviceRequest->property;
+
+        return $this->subject('A service request made for your unit')->view('service-request.created.index')
+            ->with(['serviceRequest' => $this->serviceRequest, 'user' => $user, 'unit' => $this->unit, 'category' => $category,  'property' => $property]);
+    }
+}

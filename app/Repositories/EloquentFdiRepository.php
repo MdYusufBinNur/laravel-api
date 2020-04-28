2cc0cba9ddbcbd65bcdcb03740cb2e91826a69e5
Shahriar <me@masum.ninja>
2020-04-28 22:08:10 +0600

    without pagination in fdi

--- app/Repositories/EloquentFdiRepository.php
+++ app/Repositories/EloquentFdiRepository.php
@@ -98,7 +98,12 @@ class EloquentFdiRepository extends EloquentBaseRepository implements FdiReposit
         $orderBy = !empty($searchCriteria['order_by']) ? $searchCriteria['order_by'] : 'id';
         $orderDirection = !empty($searchCriteria['order_direction']) ? $searchCriteria['order_direction'] : 'desc';
         $queryBuilder->orderBy($orderBy, $orderDirection);
-        return $queryBuilder->paginate($limit);
+
+        if (empty($searchCriteria['withOutPagination'])) {
+            return $queryBuilder->paginate($limit);
+        } else {
+            return $queryBuilder->get();
+        }
     }
 
     /**

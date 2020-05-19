ac3d78e436d07a070b936c47cd638c41cbfcdf27
Shahriar <me@masum.ninja>
2020-05-19 15:49:30 +0600

    broadcast user's profile pic and avatar

--- app/Events/Post/PostCreatedEvent.php
+++ app/Events/Post/PostCreatedEvent.php
@@ -65,7 +65,7 @@ class PostCreatedEvent implements ShouldBroadcast
      */
     public function broadcastWith()
     {
-        request()->merge(['include' => 'post.createdByUser,post.details,post.attachments,post.commentsCount']);
+        request()->merge(['include' => 'post.createdByUser,post.details,post.attachments,post.commentsCount,user.profilePic,image.avatar']);
         return [
             'post' => new PostResource($this->post)
         ];

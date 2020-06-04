b44299c7646b70d1feed2560b61d977da5d786e3
Shahriar <me@masum.ninja>
2020-06-04 18:47:57 +0600

    search issue fix with id

--- app/Repositories/EloquentBaseRepository.php
+++ app/Repositories/EloquentBaseRepository.php
@@ -316,9 +316,9 @@ class EloquentBaseRepository implements BaseRepository
         foreach ($searchCriteria as $key => $value) {
 
             // replace id with uuid when non numeric
-            if ($key == 'id' && !is_numeric($value)) {
+            /*if ($key == 'id' && !is_numeric($value)) {
                 return $key = 'uuid';
-            }
+            }*/
 
             //skip pagination related query params
             if (in_array($key, ['page', 'per_page', 'order_by', 'order_direction'])) {

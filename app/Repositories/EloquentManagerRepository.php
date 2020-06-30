a178a3a0186c8a13719a261686eb049b811a60f4
Shahriar <me@masum.ninja>
2020-06-30 20:06:32 +0600

    fixed staff query issue

--- app/Repositories/EloquentManagerRepository.php
+++ app/Repositories/EloquentManagerRepository.php
@@ -35,8 +35,10 @@ class EloquentManagerRepository extends EloquentBaseRepository implements Manage
             $queryBuilder->where($thisTable . '.propertyId', $searchCriteria['propertyId']);
         }
         if (isset($searchCriteria['query'])) {
-            $queryBuilder->where($thisTable . '.contactEmail', 'like', '%' . $searchCriteria['query'] . '%')
-                ->orWhere($thisTable . '.title', 'like', '%' . $searchCriteria['query'] . '%');
+            $queryBuilder = $queryBuilder->where(function ($query) use ($searchCriteria, $thisTable) {
+                $query->where($thisTable . '.contactEmail', 'like', '%' . $searchCriteria['query'] . '%')
+                    ->orWhere($thisTable . '.title', 'like', '%' . $searchCriteria['query'] . '%');
+            });
         }
 
         if (isset($searchCriteria['roleId'])) {
